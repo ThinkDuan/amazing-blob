8c604c6d99120f56e32edfd97bd78a06ffd9540d
duanxinxin <daunxinxins@163.com>
2018-12-06 17:40:01 +0800

    init

---
+++ src/components/page/page.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    {{this.articles}}
+  </div>
+</template>
+<script>
+export default {
+  data(){
+    return {
+      articles: ""
+    }
+  },
+  methods:{
+  },
+  created(){}
+}
+</script>
+
